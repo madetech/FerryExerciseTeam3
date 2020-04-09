fb0da0dd40ba622624b3c479d491de209caed75c
George <george.taylor@madetech.com>
2020-04-09 12:22:51 +0100

    adding in validators for the inputs

--- ferry.py
+++ ferry.py
@@ -18,7 +18,17 @@ def hello():
 @app.route('/_add_numbers')
 def add_numbers():
     corn = request.args.get('corn', 0, type=int)
+    geese = request.args.get('geese', 0, type=int)
     price = request.args.get('price', 0, type=float)
-    return jsonify(result=corn*price,price=(corn*price))
+
+    if corn < 0 or geese <0 :
+        return jsonify(error=1,error_message="Only positive geese and corn pls")
+    if corn > 0 and geese > 2 :
+        return jsonify(error=1,error_message="Too many geese to make this trip possible with the corn!")
+    if corn > 2 and geese > 0 :
+        return jsonify(error=1,error_message="Too much corn to make this trip possible with the goose!")
+
+    return jsonify(error=0,price=(corn*price))
+
 if __name__ == '__main__':
     main()
--- test_ferry.py
+++ test_ferry.py
@@ -15,3 +15,15 @@ def client():
 def test_calculate_trip_price_of_geese(client):
     rv = client.get('/_add_numbers?corn=1&price=0.25')
     assert rv.json['price'] == 0.25
+
+@pytest.mark.parametrize("corn,geese,price,error",[
+    [5,0,0.25,0],
+    [5,2,0.25,1],
+    [1,3,0.25,1],
+    [0,5,0.25,0],
+    [5,5,0.25,1],
+    [5,-5,0.25,1]])
+def test_errors_for_valid_config(client,corn,geese,price,error):
+    rv = client.get("/_add_numbers?corn={}&geese={}&price={}".format(corn,geese,price))
+    assert rv.json["error"] == error
+
